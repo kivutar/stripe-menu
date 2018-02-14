786d6d136e63b933caaaee81bb8c074652dfd0f4
Rob Loach <robloach@gmail.com>
2018-02-14 03:44:40 -0500

    Half the screen size

--- conf.lua
+++ conf.lua
@@ -1,8 +1,8 @@
 SCREEN_HEIGHT = 1080
 
 function love.conf(t)
-  t.window.width = 1920
-  t.window.height = SCREEN_HEIGHT
+  t.window.width = 1920 / 2
+  t.window.height = SCREEN_HEIGHT / 2
   t.window.msaa = 2
   t.window.highdpi = true
 end
