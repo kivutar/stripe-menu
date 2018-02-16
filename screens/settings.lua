a41cbea633b438018123c76e8db369bd218cd68f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-02-16 15:26:53 +0700

    Placement of the settings cursor

--- screens/settings.lua
+++ screens/settings.lua
@@ -1,3 +1,31 @@
+function draw_settings_cursor()
+  love.graphics.setBlendMode("add")
+
+  local a = (math.cos(t * 5) + 1) * 128 + 128
+  love.graphics.setColor(255, 255, 255, math.min(a, 255))
+  love.graphics.rectangle("line", 300-20, 42, 1350, 100)
+  love.graphics.setColor(255, 255, 255, math.min(16, 255))
+  love.graphics.rectangle("fill", 300-20, 42, 1350, 100)
+
+  love.graphics.setBlendMode("alpha")
+end
+
+function draw_settings_string(setting, i)
+  love.graphics.printf(setting.value, 300, 60 + (i - 1) * 100, 800, "right")
+end
+
+function draw_settings_bool(setting, i)
+  if setting.value then
+    love.graphics.draw(bool_on, 1460, 92 + (i - 1) * 100, 0, 0.7, 0.7, 0, bool_on:getHeight()/2)
+  else
+    love.graphics.draw(bool_off, 1460, 92 + (i - 1) * 100, 0, 0.7, 0.7, 0, bool_on:getHeight()/2)
+  end
+end
+
+function draw_settings_list(setting, i)
+  love.graphics.printf("◀ " .. setting.options[setting.value] .. " ▶", 300, 60 + (i - 1) * 100, 1310, "right")
+end
+
 function draw_settings()
   love.graphics.push()
   love.graphics.translate(0, settings_container.y)
@@ -12,30 +40,18 @@ function draw_settings()
     for i = 1, #list do
       love.graphics.print(list[i].title, 300, 60 + (i - 1) * 100)
       if list[i].type == "string" then
-        love.graphics.printf(list[i].value, 300, 60 + (i - 1) * 100, 800, "right")
+        draw_settings_string(list[i], i)
       elseif list[i].type == "bool" then
-        if list[i].value then
-          love.graphics.draw(bool_on, 1460, 92 + (i - 1) * 100, 0, 0.7, 0.7, 0, bool_on:getHeight()/2)
-        else
-          love.graphics.draw(bool_off, 1460, 92 + (i - 1) * 100, 0, 0.7, 0.7, 0, bool_on:getHeight()/2)
-        end
+        draw_settings_bool(list[i], i)
       elseif list[i].type == "list" then
-        love.graphics.printf("◀ " .. list[i].options[list[i].value] .. " ▶", 300, 60 + (i - 1) * 100, 1310, "right")
+        draw_settings_list(list[i], i)
       end
     end
   else
     love.graphics.print("Empty", 300, 60)
   end
 
-  love.graphics.setBlendMode("add")
-
-  local a = (math.cos(t * 5) + 1) * 128 + 128
-  love.graphics.setColor(255, 255, 255, math.min(a, 255))
-  love.graphics.rectangle("line", 160, SCREEN_HEIGHT / 2 - 50, cursor.width, 100)
-  love.graphics.setColor(255, 255, 255, math.min(16, 255))
-  love.graphics.rectangle("fill", 160, SCREEN_HEIGHT / 2 - 50, cursor.width, 100)
-
-  love.graphics.setBlendMode("alpha")
+  draw_settings_cursor()
 
   love.graphics.pop()
 end
