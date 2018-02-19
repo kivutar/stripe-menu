ca1115f22e24a3b5263fd58a366f623f0413cce4
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-02-19 17:36:23 +0700

    Unhardcode flags

--- flags.lua
+++ flags.lua
@@ -1,36 +1,29 @@
 flags = {}
 
 function load_flags()
-  flags["Japan"] = love.graphics.newImage("flags/Japan.png")
-  flags["Europe"] = love.graphics.newImage("flags/Europe.png")
-  flags["USA"] = love.graphics.newImage("flags/USA.png")
-  flags["Fr"] = love.graphics.newImage("flags/Fr.png")
-  flags["Es"] = love.graphics.newImage("flags/Es.png")
-  flags["En"] = love.graphics.newImage("flags/En.png")
-  flags["De"] = love.graphics.newImage("flags/De.png")
-  flags["It"] = love.graphics.newImage("flags/It.png")
-  flags["Pt"] = love.graphics.newImage("flags/Pt.png")
-  flags["Nl"] = love.graphics.newImage("flags/Nl.png")
-  flags["Ja"] = love.graphics.newImage("flags/Ja.png")
-  flags["Sv"] = love.graphics.newImage("flags/Sv.png")
-  flags["No"] = love.graphics.newImage("flags/No.png")
-  flags["World"] = love.graphics.newImage("flags/World.png")
-  flags["Brazil"] = love.graphics.newImage("flags/Brazil.png")
-  flags["France"] = love.graphics.newImage("flags/France.png")
-  flags["Korea"] = love.graphics.newImage("flags/Korea.png")
-  flags["Taiwan"] = love.graphics.newImage("flags/Taiwan.png")
+  local images = love.filesystem.getDirectoryItems("flags")
+  for k, image in ipairs(images) do
+    name = image:match("^(.+).png$")
+    if name then
+      flags[name] = love.graphics.newImage("flags/"..image)
+    end
+  end
 end
 
+-- Loop over all the games, and build an array of country flags for each game
 function flag_games()
   for h = 1, #tabs do
     local list = tabs[h].children
     for i = 1, #list do
       list[i].flags = {}
+      -- capture content of parenthesis
       for capture in list[i].title:gmatch("%s%((.-)%)") do
+        -- split on comma
         for word in capture:gmatch("([^, ]+)") do
           table.insert(list[i].flags, word)
         end
       end
+      -- remove the parenthesis from the game title
       list[i].title = list[i].title:gsub("%s%((.-)%)", "")
       list[i].flag_alpha = 0
     end
